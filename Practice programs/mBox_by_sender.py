23b721ce720f203369002a0f9d8671e7a48cc4fb
Hudson H <hudsonhargrove01@gmail.com>
2023-07-21 23:29:11 -0500

    recreated the max statement with tuples and  #

--- Practice programs/mBox_by_sender.py
+++ Practice programs/mBox_by_sender.py
@@ -9,8 +9,24 @@ for line in fhand:
     sender[sendHand] = sender.get(sendHand,0) + 1
 most = 0
 sendHand = None
-for key in sender:
+
+lst = list()
+for k,v in sender.items():
+    #creates a list of tuples with the
+    #key:value pairs in reverse order
+    lst.append( (v,k) )
+#sorts the list with from high to low
+lst = sorted(lst,reverse=True)
+# resets the list to proper key:value order
+for v,k in lst[:1]:
+    print(k,v)
+
+
+
+#Old code showing how to use a max loop
+#to find the greatest number of messages
+'''for key in sender:
     if sender.get(key,0) > most:
         most = sender.get(key,0)
         sendHand = key
-print(sendHand, most)
+print(sendHand, most)'''
