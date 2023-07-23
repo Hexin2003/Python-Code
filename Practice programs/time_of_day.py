af812a07da8bbf0ca2e1e03c693a0a3bdff611a9
Hudson H <hudsonhargrove01@gmail.com>
2023-07-22 23:22:45 -0500

    updated to search via regex

--- Practice programs/time_of_day.py
+++ Practice programs/time_of_day.py
@@ -1,14 +1,11 @@
+import re
 fhand = open('Files To Read/mbox-short.txt')
 time = dict()
-t = []
 for line in fhand:
-    if not line.startswith('From '): continue
-    words = line.split()
-    if len(words) == 0: continue
-    fullTime = words[5]
-    hour = fullTime.split(':')
-    hour = hour[0]
-    time[hour] = time.get(hour,0) + 1
+    words = line.rstrip()
+    hour = str(re.findall('^From .* ([0-9][0-9]):',words))[2:4]
+    if len(hour) > 0:
+        time[hour] = time.get(hour,0) + 1
 lst = list()
 #sorts the dictonary by keys
 for k,v in sorted(time.items()):
